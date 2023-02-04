72ce6ab5cf45a740366d891ce0332d81b59572e5
AchitaAct <97459756+AchitaAct@users.noreply.github.com>
2023-02-05 06:13:04 +0700

    Add files via upload

---
+++ login program.py
@@ -0,0 +1,6 @@
+username=str(input("Please enter your username:"))
+password=str(input("Please enter your password"))
+if(username=="Achita"  and   password=="123456"):
+  print("Welcome to Program")
+else:
+  print("The username or password is incorrect.")
